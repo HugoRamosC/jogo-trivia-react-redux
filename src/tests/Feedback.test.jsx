c6b5a4866d6f4181cc0f6b6efaf66942367500aa
Matheus Almeida <matheus.almeida.neves@gmail.com>
2022-11-11 15:02:55 -0300

    feat: add tests to Feedback page

--- src/tests/Feedback.test.jsx
+++ src/tests/Feedback.test.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, act } from '@testing-library/react';
 import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
-import Ranking from '../pages/Ranking';
-import Game from '../pages/Game';
 
 describe('Testa a funcionalidade da Ranking Page', () => {
   test('Teste se ao clicar em Ranking a rota está correta', async () => {
-    const { history } =renderWithRouterAndRedux(<Game />);
+    const { history } =renderWithRouterAndRedux(<App />);
+
+    act(() => 
+      { history.push('/feedback'); });
 
     const button = screen.getByTestId('btn-ranking')
     userEvent.click(button)
     expect(history.location.pathname).toBe('/ranking')
   });
 
-  test('Teste se ao clicar em Ranking a rota está correta', async () => {
-    const { history } =renderWithRouterAndRedux(<Game />);
+  test('Teste se ao clicar em Play Again a rota está correta', async () => {
+    const { history } =renderWithRouterAndRedux(<App />);
 
-    const button = screen.getByTestId('btn-ranking')
+    act(() => 
+      { history.push('/feedback'); });
+
+    const button = screen.getByTestId('btn-play-again')
     userEvent.click(button)
-    expect(history.location.pathname).toBe('/ranking')
+    expect(history.location.pathname).toBe('/')
   });
 });
